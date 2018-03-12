b081d153f78e7ac4b4cc26b5f29a137659c3437c
Conor Neilson <condwanaland@gmail.com>
2018-03-12 21:16:13 +1300

    add extract d5

---
+++ R/extract.R
@@ -0,0 +1,14 @@
+#' extract_d_matrix
+#'
+#' Load the dihedral D5 matrix into memory. One of the matrices used in
+#' check digit calculation
+#'
+#' @return A dataframe
+#' @export
+#'
+#' @examples
+#' extract_d_matrix()
+
+extract_d_matrix <- function(){
+  mat_d
+}
