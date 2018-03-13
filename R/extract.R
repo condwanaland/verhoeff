e3fd371fe3df3d22d4f9e071e5f69d73826e72bb
Conor Neilson <condwanaland@gmail.com>
2018-03-13 17:26:50 +1300

    adds all matrices and closes #2

--- R/extract.R
+++ R/extract.R
@@ -10,5 +10,54 @@
 #' extract_d_matrix()
 
 extract_d_matrix <- function(){
-  mat_d
+  d5_matrix
+}
+
+
+#' extract_f_matrix
+#'
+#' Load the F matrix into memory. One of the matrices used in
+#' check digit calculation
+#'
+#' @return A dataframe
+#' @export
+#'
+#' @examples
+#' extract_f_matrix()
+
+extract_f_matrix <- function(){
+  f_matrix
+}
+
+
+#' extract_inv_vector
+#'
+#' Load the INV matrix into memory. One of the matrices used in
+#' check digit calculation
+#'
+#' @return A dataframe
+#' @export
+#'
+#' @examples
+#' extract_inv_matrix()
+
+extract_inv_matrix <- function(){
+  inv_vector
+}
+
+
+#' load_matrices
+#'
+#' Add all matrices for verhoeff calculation into memory
+#'
+#' @return Three dataframes
+#' @export
+#'
+#' @examples
+#' load_matrices()
+
+load_matrices <- function(){
+  d5_matrix
+  f_matrix
+  inv_vector
 }
